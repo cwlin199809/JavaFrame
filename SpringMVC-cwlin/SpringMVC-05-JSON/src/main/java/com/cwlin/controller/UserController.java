d8df30caac882aac27e936a7f771a37036625b93
醉染 <cwlin1998@foxmail.com>
2021-04-23 17:35:47 +0800

    2021.04.23 17：35

--- SpringMVC-cwlin/SpringMVC-05-JSON/src/main/java/com/cwlin/controller/UserController.java
+++ SpringMVC-cwlin/SpringMVC-05-JSON/src/main/java/com/cwlin/controller/UserController.java
@@ -1,22 +1,91 @@
 package com.cwlin.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.cwlin.pojo.User;
+import com.cwlin.utils.JsonUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
 public class UserController {
     @RequestMapping("/j1")
-    @ResponseBody //此时不会被视图解析器解析，会直接返回一个字符串
+    //@ResponseBody //此时不会被视图解析器解析，会直接返回一个字符串
     public String json1() throws JsonProcessingException {
-        //jackson, ObjectMapper
+        //创建一个jackson的对象映射器，用来解析数据
         ObjectMapper mapper = new ObjectMapper();
         //创建一个对象
         User user = new User("cwlin",3,"男");
+        //将我们的对象解析成为json格式
+        String str = mapper.writeValueAsString(user);
+        //由于@ResponseBody注解，这里会将str转成json格式返回；十分方便
+        return str;
+    }
 
-        return mapper.writeValueAsString(user);
+    @RequestMapping("/j2")
+    public String json2() throws JsonProcessingException {
+        //创建一个jackson的对象映射器，用来解析数据
+        ObjectMapper mapper = new ObjectMapper();
+        //创建一个对象
+        List<User> userList = new ArrayList<User>();
+        User user1 = new User("cwlin1",3,"男");
+        User user2 = new User("cwlin2",3,"男");
+        User user3 = new User("cwlin3",3,"男");
+        User user4 = new User("cwlin4",3,"男");
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+        //将我们的对象解析成为json格式
+        String str = mapper.writeValueAsString(userList);
+        //由于@ResponseBody注解，这里会将str转成json格式返回；十分方便
+        return str;
+    }
+
+    @RequestMapping("/j3")
+    public String json3() throws JsonProcessingException {
+        //创建一个jackson的对象映射器，用来解析数据
+        ObjectMapper mapper = new ObjectMapper();
+        //创建一个对象
+        Date date = new Date();
+        //将我们的对象解析成为json格式
+        return mapper.writeValueAsString(date);
+    }
+
+    @RequestMapping("/j4")
+    public String json4() throws JsonProcessingException {
+        //创建一个jackson的对象映射器，用来解析数据
+        ObjectMapper mapper = new ObjectMapper();
+
+        //不使用时间戳的方式
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        //自定义日期格式对象
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //指定日期格式
+        mapper.setDateFormat(sdf);
+
+        //创建一个对象
+        Date date = new Date();
+        //将我们的对象解析成为json格式
+        return mapper.writeValueAsString(date);
+    }
+
+    @RequestMapping("/j5")
+    public String json5() {
+        //创建一个对象
+        Date date = new Date();
+        //将我们的对象解析成为json格式
+        return JsonUtils.getJson(date);
+        //return JsonUtils.getJson(date,"yyyy-MM-dd HH:mm:ss");
     }
 }
