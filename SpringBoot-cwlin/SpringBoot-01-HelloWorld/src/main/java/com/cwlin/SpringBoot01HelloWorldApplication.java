ea057916b0950cc14393d720c7b62190a6ceaafb
醉染 <cwlin1998@foxmail.com>
2021-07-20 01:32:02 +0800

    2021.07.20 01：30

--- SpringBoot-cwlin/SpringBoot-01-HelloWorld/src/main/java/com/cwlin/SpringBoot01HelloWorldApplication.java
+++ SpringBoot-cwlin/SpringBoot-01-HelloWorld/src/main/java/com/cwlin/SpringBoot01HelloWorldApplication.java
@@ -3,12 +3,13 @@ package com.cwlin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//程序的主启动类
+//@SpringBootApplication 来标注一个主程序类，启动类下的所有资源被导入
+//说明这是一个Spring Boot应用
 @SpringBootApplication
 public class SpringBoot01HelloWorldApplication {
 
     public static void main(String[] args) {
-        //SpringApplication
+        //以为是启动了一个方法，实际上是启动了一个服务
         SpringApplication.run(SpringBoot01HelloWorldApplication.class, args);
     }
 
--- SpringBoot-cwlin/SpringBoot-01-HelloWorld/src/main/java/com/cwlin/controller/HelloController.java
+++ SpringBoot-cwlin/SpringBoot-01-HelloWorld/src/main/java/com/cwlin/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.cwlin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
+    @GetMapping("/h1")
+    @ResponseBody
     public String hello() {
         return "Hello World";
     }
